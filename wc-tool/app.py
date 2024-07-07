212e35089e45ba9bbb26acc086b57255dbe9e33a
aakashb95 <aakashbakhle@gmail.com>
2024-07-07 21:02:12 +0530

    add: count lines

--- wc-tool/app.py
+++ wc-tool/app.py
@@ -5,16 +5,23 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("filename")
 parser.add_argument("-c", "--count", action="store_true", help="Count bytes")
-
+parser.add_argument("-l", "--lines", action="store_true", help="Count lines")
 
 def count_bytes(content):
+    with open(filename, "rb") as f:
+        content = f.read()
     return len(content)
+
+def count_lines(content):
+    with open(filename, "r") as f:
+        content = f.read()
+    return len(content.splitlines())
 
 if __name__ == "__main__":
     args = parser.parse_args()
     filename = args.filename
-    with open(filename, "rb") as f:
-        content = f.read()
 
     if args.count:
-        print(f"{count_bytes(content)} {filename}")
+        print(f"{count_bytes(filename)} {filename}")
+    if args.lines:
+        print(f"{count_lines(filename)} {filename}")
