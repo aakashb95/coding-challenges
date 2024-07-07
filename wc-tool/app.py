dec1f87bfd190e2dc8a8ff14fdcf408eff3f301a
aakashb95 <aakashbakhle@gmail.com>
2024-07-07 22:46:41 +0530

    add: types and refactor spaces

--- wc-tool/app.py
+++ wc-tool/app.py
@@ -11,19 +11,19 @@ parser.add_argument("-w", "--words", action="store_true", help="Count words")
 parser.add_argument("-m", action="store_true")
 
 
-def count_bytes(content):
+def count_bytes(content: bytes) -> int:
     return len(content)
 
-def count_lines(content):
+def count_lines(content: bytes) -> int:
     return len(content.splitlines())
 
-def count_words(content):
+def count_words(content: bytes) -> int:
     return len(content.split())
 
-def count_chars(content):
+def count_chars(content: bytes) -> int:
     return len(content.decode('utf-8-sig')) + 1 # (for Byte Order Mark). Used chardet to check the encoding
 
-def count_all(content):
+def count_all(content: bytes) -> dict:
     return {
         "counts": count_bytes(content),
         "lines": count_lines(content),
@@ -41,13 +41,13 @@ if __name__ == "__main__":
             content = f.read()
 
     if args.count:
-        print(f"    {count_bytes(content)} {filename}")
+        print(f"{count_bytes(content):8d} {filename}")
     elif args.lines:
-        print(f"    {count_lines(content)} {filename}")
+        print(f"{count_lines(content):8d} {filename}")
     elif args.words:
-        print(f"    {count_words(content)} {filename}")
+        print(f"{count_words(content):8d} {filename}")
     elif args.m:
-        print(f"    {count_chars(content)} {filename}")
+        print(f"{count_chars(content):8d} {filename}")
     else:
         stats = count_all(content)
-        print(f"    {stats['lines']}   {stats['words']}  {stats['counts']} {filename}")
+        print(f"{stats['lines']:8d}{stats['words']:8d}{stats['counts']:8d} {filename}")
