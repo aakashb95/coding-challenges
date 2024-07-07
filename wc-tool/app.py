a56afc46df91942d729c369faeb9f1f9401d6474
aakashb95 <aakashbakhle@gmail.com>
2024-07-07 20:50:50 +0530

    mod: make file open common

--- wc-tool/app.py
+++ wc-tool/app.py
@@ -7,15 +7,14 @@ parser.add_argument("filename")
 parser.add_argument("-c", "--count", action="store_true", help="Count bytes")
 
 
-def count_bytes(filename):
-    with open(filename, "rb") as f:
-        content = f.read()
-
+def count_bytes(content):
     return len(content)
-
 
 if __name__ == "__main__":
     args = parser.parse_args()
     filename = args.filename
+    with open(filename, "rb") as f:
+        content = f.read()
+
     if args.count:
-        print(f"{count_bytes(filename)} {filename}")
+        print(f"{count_bytes(content)} {filename}")
